8a260c40686cd6b0096b66af5be639f3bf612c01
JonKappa <cappytwoj@gmail.com>
2021-11-09 13:15:46 -0500

    f1

--- src/main/java/Tables.java
+++ src/main/java/Tables.java
@@ -1,13 +1,34 @@
 public class Tables {
     public String getSmallMultiplicationTable() {
-        return null;
+        StringBuilder table = new StringBuilder();
+        for (int i = 1; i <=5; i++) {
+            for (int j = 1; j <=5; j++) {
+                table.append(String.format("%5s", (i * j) + " |"));
+            }
+            table.append("\n");
+        }
+        return table.toString();
     }
 
     public String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder table = new StringBuilder();
+        for (int i = 1; i <=10; i++) {
+            for (int j = 1; j <=10; j++) {
+                table.append(String.format("%5s", (i * j) + " |"));
+            }
+            table.append("\n");
+        }
+        return table.toString();
     }
 
     public String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder table = new StringBuilder();
+        for (int i = 1; i <=tableSize; i++) {
+            for (int j = 1; j <=tableSize; j++) {
+                table.append(String.format("%5s", (i * j) + " |"));
+            }
+            table.append("\n");
+        }
+        return table.toString();
     }
 }
